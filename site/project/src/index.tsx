495da5fe6759cacb747e1020de487da138ee887f
Mathis Granier <mathis.granier@etu.unilim.fr>
2025-09-19 23:02:14 +0200

    suppression d'un fichier indésirable

--- site/project/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { APropos } from "./screens/APropos/APropos";
-
-createRoot(document.getElementById("app") as HTMLElement).render(
-  <StrictMode>
-    <APropos />
-  </StrictMode>,
-);
