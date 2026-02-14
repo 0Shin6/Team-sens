60ffe4416fd607c2c55959c6fb2c6e57e41b6b06
Mathis Granier <mathis.granier@etu.unilim.fr>
2026-02-14 11:21:41 +0100

    Ajout de la commande de retrait de news

--- backend/bot/bot.py
+++ backend/bot/bot.py
@@ -1,10 +1,11 @@
-from http import client
 import sqlite3
 import discord
+from discord.ext import commands
 
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 conn = sqlite3.connect('../teamsens.db')
 
-@client.tree.command(name="add_news", description="Ajouter une actualité")
+@bot.tree.command(name="add_news", description="Ajouter une actualité")
 async def add_news(interaction: discord.Interaction, titre: str, image: str, desc: str):
     conn = sqlite3.connect('../teamsens.db')
     c = conn.cursor()
@@ -12,3 +13,12 @@ async def add_news(interaction: discord.Interaction, titre: str, image: str, des
     conn.commit()
     conn.close()
     await interaction.response.send_message(f" Actualité '{titre}' ajoutée !")
+
+@bot.tree.command(name="supp_news", description="Retirer une actualité")
+async def supp_news(interaction: discord.Interaction, titre:  str):
+    conn = sqlite3.connect('../teamsens.db')
+    c = conn.cursor()
+    c.execute("DELETE FROM news WHERE titre = (?)", (titre))
+    conn.commit()
+    conn.close()
+    await interaction.response.send_message(f" Actualité '{titre}' retiré !")
